87d17595f9d7582cfdf3c793175ca938bbc1cf4b
hideki0324 <hideki19850324@gmail.com>
2018-03-30 17:21:56 -0700

    fixed

--- Assessment2_Hideki_Murata/controls.js
+++ Assessment2_Hideki_Murata/controls.js
@@ -57,46 +57,109 @@ range4.addEventListener("change", function(){
 var hair = document.getElementById("hair"),
     eyes = document.getElementById("eyes"),
     nose = document.getElementById("nose"),
-    mouth = document.getElementById("mouth");
-
-function randomFace(){
-    hair.src = "img/hair"+rando+".png";
-    eyes.src = "img/eyes"+rando+".png";
-    nose.src = "img/nose"+rando+".png";
-    mouth.src = "img/mouth"+rando+".png";
-}
+    mouth = document.getElementById("mouth"),
+    num = 0;
 
 hair.addEventListener("click", function(){
-    hairchange += 1;
-    hair.src = "img/hair"+hairchange+".png";
-    if(hairchange >=3){
-        hairchange = 0;
+    num += 1;
+    hair.src = "img/hair"+num+".png";
+    if(num >=3){
+        num = 0;
     }
-});
+})
 
 eyes.addEventListener("click", function(){
-    hairchange += 1;
-    eyes.src = "img/eyes"+hairchange+".png";
-    if(hairchange >=3){
-        hairchange = 0;
+    num += 1;
+    eyes.src = "img/eyes"+num+".png";
+    if(num >=3){
+        num = 0;
     }
-});
+})
 
 nose.addEventListener("click", function(){
-    hairchange += 1;
-    nose.src = "img/nose"+hairchange+".png";
-    if(hairchange >=3){
-        hairchange = 0;
+    num += 1;
+    nose.src = "img/nose"+num+".png";
+    if(num >=3){
+        num = 0;
     }
-});
+})
 
 mouth.addEventListener("click", function(){
-    hairchange += 1;
-    mouth.src = "img/mouth"+hairchange+".png";
-    if(hairchange >=3){
-        hairchange = 0;
+    num += 1;
+    mouth.src = "img/mouth"+num+".png";
+    if(num >=3){
+        num = 0;
     }
+})
+
+//LEVEL 4a - CREATING ELEMENTS - /3 - controls.js
+var plus = document.getElementById("plus"),
+    dispDiv = document.getElementById("display"),
+    iw = 100,
+    ih = 100;
+
+document.getElementById("plus").addEventListener("click", function(){
+    var newDiv = document.createElement("div");
+    document.body.appendChild(newDiv);
+    newDiv.className = "boxes";
+    newDiv.id = "box"+num;
+    newDiv.style.width = iw+"px";
+    newDiv.style.height = iw+"px";
+        
+    iw -= 10;
+    ih -= 10;
+    if(iw ,=0){
+        iw = 100;
+    }
+    if(ih ,=0){
+        ih = 100;
+    }  
+    num++;
 });
+
+
+//LEVEL 4b - RANDOM - /2 - controls.js
+
+function randomFace(){
+    var rando = Math.round(Math.random()*2)+1;
+    var randohairsize = Math.round(Math.random()*70);
+    var randoeyesize = Math.round(Math.random()*40);
+    var randonosesize = Math.round(Math.random()*30);
+    var randomouthsize = Math.round(Math.random()*35);
+    var r = Math.round(Math.random()*255);
+    var g = Math.round(Math.random()*255);
+    var b = Math.round(Math.random()*255);
+    var preview.style.backgroundColor = "rgb("+r+","+g+","+b+")";
+}
+    
+autoRandom.addEventListener("click", function(){
+   timer =  setInterval(function(){
+		    randomFace();
+        createFace();
+    },500);
+});
+
+
+//LEVEL 5a - FUNCTIONS FOR LEVEL 4 - /1
+//LEVEL 5b - FUNCTION FOR LEVEL 2b - /1
+
+//LEVEL 6 - INTERVAL CREATING - /1 - controls.js
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
